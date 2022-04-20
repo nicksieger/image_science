c2852255ce9f58d5a1b49814523a080242cebe71
Nick Sieger <nick@nicksieger.com>
2022-04-20 14:54:39 -0500

    Ignore warnings from recent clang

--- ext/image_science/extconf.rb
+++ ext/image_science/extconf.rb
@@ -3,6 +3,8 @@ require 'mkmf'
 include_path = '/usr/local/include:/usr/include:/opt/local/include:/opt/include'
 lib_path = '/usr/local/lib:/usr/lib:/opt/local/lib:/opt/lib'
 
+append_cflags(%w[-Wno-parentheses -Wno-shorten-64-to-32 -Wno-implicit-function-declaration -Wno-compound-token-split-by-macro])
+
 dir_config("image_science", include_path, lib_path)
 
 have_library('freeimage')
